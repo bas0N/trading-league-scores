cc3bc498f663d8fa85e15748c6d19db95031bb9a
basON <wch.basinski@gmail.com>
2023-03-05 23:52:03 +0100

    Add basic cron job

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from "express";
-
+import cron from "node-cron";
+require("dotenv").config();
 console.log("eloelo");
 const app = express();
+cron.schedule("*/15 * * * * *", function () {
+  console.log("---------------------");
+  console.log("running a task every 15 seconds");
+});
 app.get("/", (req: Request, res: Response) => {
   console.log("siemanko");
   res.send({ message: "siemanko" });
 });
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port: ${process.env.PORT}`);
 });
